e8e24487c5de73ac6e3311f96e1a6928dfef8797
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2024-03-21 04:34:41 +0900

    Add VPC configuration

--- index.ts
+++ index.ts
@@ -3,7 +3,23 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// const bucket = new aws.s3.Bucket("my-bucket");
 
 // Export the name of the bucket
-export const bucketName = bucket.id;
+// export const bucketName = bucket.id;
+
+const vpc = new awsx.ec2.Vpc("pulumi-vpc", {
+  subnetSpecs: [
+    {
+      type: awsx.ec2.SubnetType.Public,
+      cidrMask: 24,
+    },
+    {
+      type: awsx.ec2.SubnetType.Private,
+      cidrMask: 24,
+    },
+  ],
+  natGateways: {
+    strategy: awsx.ec2.NatGatewayStrategy.None,
+  },
+});
